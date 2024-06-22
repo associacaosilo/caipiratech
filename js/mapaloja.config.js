6ce4494c36a72c70eb0aeee10da75d5fe5162017
Lucas Lago <lago@nic.br>
2024-06-22 16:41:17 -0300

    tentando aplicar estilo

--- js/mapaloja.config.js
+++ js/mapaloja.config.js
@@ -2,7 +2,8 @@
 
 // eslint-disable-next-line no-unused-vars
 const config = {
-  style: 'mapbox://styles/mapbox/streets-v12',
+  // style: 'mapbox://styles/mapbox/streets-v12',
+  style: 'mapbox://styles/lucaslago/clxqgg73103bw01qjf4dyfucw',
   accessToken:
     'pk.eyJ1IjoibHVjYXNsYWdvIiwiYSI6ImNseHFmMW92eDBhbHcya29qZTdnY2NjOXEifQ.kubyan0dGC6Gpff6NrhS1g',
   CSV: '../mapaloja/TesteConsolidada.csv',
